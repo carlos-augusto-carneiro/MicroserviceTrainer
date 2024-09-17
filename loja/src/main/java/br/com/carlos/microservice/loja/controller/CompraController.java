74678c1bef12fd30bf1e3fa011472eb257f6e495
carlos-augusto-carneiro <devcarlosaugusto@outlook.com>
2024-09-17 10:35:33 -0300

    Added CompraController

--- loja/src/main/java/br/com/carlos/microservice/loja/controller/CompraController.java
+++ loja/src/main/java/br/com/carlos/microservice/loja/controller/CompraController.java
@@ -1,19 +1,24 @@
 package br.com.carlos.microservice.loja.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.carlos.microservice.loja.dto.CompraDTO;
+import br.com.carlos.microservice.loja.service.CompraService;
 
 @RestController
 @RequestMapping("/compra")
 public class CompraController {
 
+    @Autowired
+    private CompraService compraService;
+
     @RequestMapping(method = RequestMethod.POST)
     public void realizaCompra(@RequestBody CompraDTO compraDTO){
-        
+        compraService.realizaCompra(compraDTO);
     }
 
 }
