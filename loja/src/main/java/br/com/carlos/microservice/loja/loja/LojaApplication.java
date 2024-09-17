db2f2d419ea9b88e52401c32bef8b9e31fa79795
carlos-augusto-carneiro <devcarlosaugusto@outlook.com>
2024-09-17 13:06:58 -0300

    Change LojaApplication

--- loja/src/main/java/br/com/carlos/microservice/loja/loja/LojaApplication.java
+++ loja/src/main/java/br/com/carlos/microservice/loja/loja/LojaApplication.java
@@ -1,4 +1,4 @@
-package br.com.carlos.microservice.loja;
+package br.com.carlos.microservice.loja.loja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
