29e25dbc8f8f504f92ae5900c6356f31265f31a3
carlos-augusto-carneiro <devcarlosaugusto@outlook.com>
2024-09-17 13:02:53 -0300

    Added InfoService in fornecedor

---
+++ fornecedor/src/main/java/br/com/carlos/microservice/fornecedor/fornecedor/service/InfoService.java
@@ -0,0 +1,19 @@
+package br.com.carlos.microservice.fornecedor.fornecedor.service;
+
+import java.lang.ProcessHandle.Info;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.carlos.microservice.fornecedor.fornecedor.repository.InfoRepository;
+
+@Service
+public class InfoService {
+
+    @Autowired
+    private InfoRepository infoRepository;
+
+    public Info getInfoPorEstado(String estado){
+        return infoRepository.findByEstado(estado);
+    }
+}
